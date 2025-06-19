7888384a9503b817fda06b0bcf8926ddb5e73408
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2025-06-20 01:15:52 +0400

    bug

--- DB/db.js
+++ DB/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection failed:', error);
